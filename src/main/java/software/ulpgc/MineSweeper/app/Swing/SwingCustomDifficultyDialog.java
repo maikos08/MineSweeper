6a642e740876ead650b05f32d9b90aaba62f1ae6
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2025-01-15 11:43:20 +0000

    Descripción de los cambios

--- src/main/java/software/ulpgc/MineSweeper/app/Swing/SwingCustomDifficultyDialog.java
+++ src/main/java/software/ulpgc/MineSweeper/app/Swing/SwingCustomDifficultyDialog.java
@@ -1,6 +1,13 @@
 package software.ulpgc.MineSweeper.app.Swing;
 
+import software.ulpgc.MineSweeper.arquitecture.model.Difficulty;
 import software.ulpgc.MineSweeper.arquitecture.view.CustomDifficultyDialog;
 
 public class SwingCustomDifficultyDialog implements CustomDifficultyDialog {
+
+    @Override
+    public Difficulty getCustomDifficulty() {
+        throw new UnsupportedOperationException("Unimplemented method 'getCustomDifficulty'");
+    }
+
 }
