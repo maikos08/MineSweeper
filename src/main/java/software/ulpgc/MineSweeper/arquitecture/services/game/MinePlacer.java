945af77eeee533bcddcb85fd7f064c44d9b075a7
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2025-01-16 00:08:21 +0000

    fix de primer click bomba

--- src/main/java/software/ulpgc/MineSweeper/arquitecture/services/game/MinePlacer.java
+++ src/main/java/software/ulpgc/MineSweeper/arquitecture/services/game/MinePlacer.java
@@ -24,4 +24,23 @@ public class MinePlacer {
 
         return cells;
     }
+
+    public Cell[][] placeMines(Cell[][] cells, int mineCount, int avoidRow, int avoidCol) {
+        int rows = cells.length;
+        int columns = cells[0].length;
+        Random random = new Random();
+
+        int placedMines = 0;
+        while (placedMines < mineCount) {
+            int x = random.nextInt(rows);
+            int y = random.nextInt(columns);
+
+            if (!cells[x][y].hasMine() && (x != avoidRow || y != avoidCol)) {
+                cells[x][y] = new Cell(true, false, false, 0);
+                placedMines++;
+            }
+        }
+
+        return cells;
+    }
 }
